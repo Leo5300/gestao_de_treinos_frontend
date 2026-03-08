3b73b9a8de5678e69a67d3bb39dfd7ddcca0c741
Leo5300 <lleonardoou89325@gmail.com>
2026-03-08 05:31:05 -0300

    fix auth proxy session

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://gestao-de-treinos.onrender.com/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
