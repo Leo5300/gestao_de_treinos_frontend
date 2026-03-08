d10669e161f3bd8a5cdfd5e443ce829cb7944222
Leo5300 <lleonardoou89325@gmail.com>
2026-03-08 02:40:35 -0300

    fix: add api proxy rewrite

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
@@ -9,6 +8,15 @@ const nextConfig: NextConfig = {
         hostname: "*.ufs.sh",
       },
     ],
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://gestao-de-treinos.onrender.com/:path*",
+      },
+    ];
   },
 };
 
