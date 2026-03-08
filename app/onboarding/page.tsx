e7c85cf0716871a10120fa99ba9aab5498a7ff76
Leo5300 <lleonardoou89325@gmail.com>
2026-03-08 05:41:09 -0300

    fix onboarding session

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,16 +1,11 @@
 import { redirect } from "next/navigation";
 import { authClient } from "@/app/_lib/auth-client";
-import { headers } from "next/headers";
 import { getHomeData, getUserTrainData } from "@/app/_lib/api/fetch-generated";
 import dayjs from "dayjs";
 import { Chat } from "@/app/_components/chat";
 
 export default async function OnboardingPage() {
-  const session = await authClient.getSession({
-    fetchOptions: {
-      headers: await headers(),
-    },
-  });
+  const session = await authClient.getSession();
 
   if (!session.data?.user) redirect("/auth");
 
